23fdaf5af61cd433c50ae133c4982d8a3aadcb5b
Alexander <40409129+a1exalexander@users.noreply.github.com>
2019-02-18 17:34:46 +0200

    update repo

--- frontend/src/i18n/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-
-Vue.i18n.add('en', require('./en.json'));
-Vue.i18n.set('en');
-Vue.i18n.fallback('en');
--- frontend/vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    outputDir: process.env.OUTPUT_DIR,
-	baseUrl: process.env.NODE_ENV === 'gh-pages' ? '/merge-draft/dist/' : '/'
-};
